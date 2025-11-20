138adfc931702a79c8dcd514f34ef7fdc29ae521
lacojoaliesma24 <lacojo_aliesma24@iesmedinaazahara.com>
2025-11-20 12:54:01 +0100

    feat: integrate Tailwind CSS and restructure app layout

---
+++ app/pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+    <h1 class="text-3xl font-bold underline">
+        Welcome to Nuxt 3 with Tailwind CSS!
+    </h1>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-
+  css: ['./app/assets/css/main.css'],
   modules: [
     '@nuxt/eslint',
     '@nuxt/image',
     '@nuxt/test-utils',
     '@nuxt/ui'
-  ]
+  ],
+  vite: {
+    plugins: [tailwindcss()]
+  }
 })
