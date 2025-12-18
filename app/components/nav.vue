e191bcd7fbb63c26142676bef48dee372bd85c99
LafuenteColoradoJose <joselafuente77@gmail.com>
2025-12-18 07:44:43 +0100

    feat: ajustar la clase de estilo en el componente de navegación para mejorar la consistencia

--- app/components/nav.vue
+++ app/components/nav.vue
@@ -1,8 +1,9 @@
+<!-- eslint-disable vue/multi-word-component-names -->
 <!-- eslint-disable vue/html-self-closing -->
 <!-- eslint-disable vue/attributes-order -->
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-  <nav class="sidebar-nav border-b md:border-b-0 md:border-r-2 w-full md:w-64 bg-surface flex-shrink-0 flex flex-col">
+  <nav class="sidebar-nav border-b md:border-b-0 md:border-r-2 w-full md:w-64 bg-surface shrink-0 flex flex-col">
     <div class="flex items-center justify-between p-4 md:flex-col md:gap-4">
       <Logo />
       <button
